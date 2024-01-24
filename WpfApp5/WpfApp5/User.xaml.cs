19349cba2a575678a8ab8d2cbf2fc0df167d90d4
stariyostin <sasha.starostin.01@gmail.com>
2024-01-24 10:12:58 +0300

    added auth

--- WpfApp5/WpfApp5/User.xaml.cs
+++ WpfApp5/WpfApp5/User.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp5
 {
 	/// <summary>
-	/// Логика взаимодействия для MainWindow.xaml
+	/// Логика взаимодействия для User.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class User : Window
 	{
-		public MainWindow()
+		public User()
 		{
 			InitializeComponent();
-		}
-
-		private void Button_Click(object sender, RoutedEventArgs e)
-		{
-			User user = new User();
-			this.Close();
-			user.Show();
 		}
 	}
 }
